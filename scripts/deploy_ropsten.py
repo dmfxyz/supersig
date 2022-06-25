a13ec6314b759719ddbab1adf482d929415d8d2e
dmfxyz <d@dmf.xyz>
2022-06-25 14:55:23 -0400

    basic deploy

---
+++ scripts/deploy_ropsten.py
@@ -0,0 +1,9 @@
+from ape import accounts, project
+
+def deploy():
+    deployer = accounts.load("ropsten")
+    signer1, signer2, signer3 = accounts.load('signer1').address, accounts.load('signer2').address, accounts.load('signer3').address
+    return deployer.deploy(project.supersig, [signer1, signer2, signer3], 3)
+
+def main():
+    deploy()
