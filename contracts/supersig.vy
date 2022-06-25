7f976494b1a4f42af256a75a2b543d488ce1b18e
dmfxyz <d@dmf.xyz>
2022-06-25 14:21:25 -0400

    payment support and revoke support

--- contracts/supersig.vy
+++ contracts/supersig.vy
@@ -5,6 +5,7 @@
 struct Proposal:
     target: address
     calldata: Bytes[20000]
+    value: uint256
 
 ## Events ##
 event Proposed:
@@ -26,19 +27,20 @@ approvals: public(HashMap[uint256, uint256])
 approved: HashMap[uint256,DynArray[address, 69]]
 
 @external
+@payable
 def __init__(_owners: DynArray[address, 69], _minimum: uint256):
     self.owners = _owners
     self.minimum = _minimum
     self.myself = self
 
 @external
-def propose(id: uint256, target: address, calldata: Bytes[20000]):
+def propose(id: uint256, target: address, calldata: Bytes[20000], _value: uint256):
     ## TODO: tFigure out how to do a zero comparison for this in vyper, 
     ## there is probably a way better way to check this
-    if self.proposals[id].target != 0x0000000000000000000000000000000000000000:
+    if self.proposals[id].target != 0x0000000000000000000000000000000000000000 or self.proposals[id].value != 0:
         raise "Proposal already exists"
 
-    self.proposals[id] = Proposal({target: target, calldata: calldata})
+    self.proposals[id] = Proposal({target: target, calldata: calldata, value: _value})
     log Proposed(msg.sender, id)
 
 @external
@@ -64,7 +66,7 @@ def approve(id: uint256):
 def execute(id: uint256):
     ## TODO: tFigure out how to do a zero comparison for this in vyper, 
     ## there is probably a way better way to check this
-    if self.proposals[id].target == 0x0000000000000000000000000000000000000000:
+    if self.proposals[id].target == 0x0000000000000000000000000000000000000000 and self.proposals[id].value == 0:
         raise "Proposal does not exist"
     
     ## Check that the proposal has been approved by the minimum number of owners
@@ -74,5 +76,29 @@ def execute(id: uint256):
     ## Execute the proposal
     ## TODO: Actually test that this return stuff works
     proposal: Proposal = self.proposals[id]
-    ret: Bytes[20000] = raw_call(proposal.target, proposal.calldata, max_outsize=20000)
+
+    ## Neutralize proposal before executing 
+    self.proposals[id] = Proposal({target: 0x0000000000000000000000000000000000000000, calldata: b'\x00', value: 0})
+    self.approvals[id] = 0
+    self.approved[id] = [0x0000000000000000000000000000000000000000]
+
+    ## Execute
+    ret: Bytes[20000] = raw_call(proposal.target, proposal.calldata, value=proposal.value, max_outsize=20000)
     log Executed(msg.sender, id)
+
+@external
+def revoke_approval(id: uint256):
+    prior_approvals: DynArray[address, 69] = self.approved[id]
+    for index in range(69):
+        if prior_approvals[index] == msg.sender:
+            prior_approvals[index] = 0x0000000000000000000000000000000000000000
+            self.approved[id] = prior_approvals
+            self.approvals[id] = self.approvals[id] - 1
+            return
+    raise "No approval to revoke"
+
+
+@external
+@payable
+def __default__():
+    pass
--- tests/test_supersig.py
+++ tests/test_supersig.py
@@ -12,7 +12,7 @@ def supersig(accounts):
 def supersig_with_proposal(supersig, accounts):
     target = "0x000000000000000000000000000000000000dead"
     calldata = "0xbeaf"
-    supersig.propose(1, target, calldata, sender=accounts[0])
+    supersig.propose(1, target, calldata, 0, sender=accounts[0])
     return supersig
 
 @pytest.fixture()
@@ -30,7 +30,7 @@ def test_init(supersig, accounts):
 def test_proposal(supersig, accounts):
     target = "0x000000000000000000000000000000000000dead"
     calldata = "0xbeaf"
-    supersig.propose(1, target, calldata, sender=accounts[0])
+    supersig.propose(1, target, calldata, 0, sender=accounts[0])
     assert supersig.proposals(1).target.lower() == target # sorry https://eips.ethereum.org/EIPS/eip-55
     assert supersig.proposals(1).calldata == HexBytes(calldata)
 
@@ -41,7 +41,7 @@ def test_approval(supersig_with_proposal, accounts):
 def test_execute(supersig, test_target_contract, accounts):
     ## "set_magic_number(69)" courtesy of cast :) 
     calldata = "0x70a5aa210000000000000000000000000000000000000000000000000000000000000045"
-    supersig.propose(2, test_target_contract, calldata, sender=accounts[0])
+    supersig.propose(2, test_target_contract, calldata, 0, sender=accounts[0])
 
     ## Approve three times
     supersig.approve(2, sender=accounts[0])
@@ -54,9 +54,35 @@ def test_execute(supersig, test_target_contract, accounts):
     ## Check that the proposal was executed
     assert test_target_contract.magic_number() == 0x45
 
+def test_revoke_approval(supersig_with_proposal, accounts):
+    supersig_with_proposal.approve(1, sender=accounts[0])
+    assert supersig_with_proposal.approvals(1) == 1
+    supersig_with_proposal.revoke_approval(1, sender=accounts[0])
+    assert supersig_with_proposal.approvals(1) == 0
+
+    ## should be able to approve again
+    supersig_with_proposal.approve(1, sender=accounts[0])
+
+def test_withdraw(supersig, accounts):
+    accounts[0].transfer(supersig, value=42)
+    assert supersig.balance == 42
+    supersig.propose(3, accounts[4].address, '0x0', 42, sender=accounts[0])
+    supersig.approve(3, sender=accounts[0])
+    supersig.approve(3, sender=accounts[1])
+    supersig.approve(3, sender=accounts[2])
+
+    prev_balance = accounts[4].balance
+    supersig.execute(3, sender=accounts[0])
+    assert accounts[4].balance == prev_balance + 42
+
+def test_deploy_with_value(accounts):
+    deployer = accounts[0]
+    contract = deployer.deploy(project.supersig, [accounts[0], accounts[1], accounts[2]], 3, value=1)
+    assert contract.balance == 1
+
 def test_fail_propose_already_exists(supersig_with_proposal, accounts):
     with pytest.raises(ContractLogicError):
-        supersig_with_proposal.propose(1, "0x000000000000000000000000000000000000dead", "0xbeaf", sender=accounts[0])
+        supersig_with_proposal.propose(1, "0x000000000000000000000000000000000000dead", "0xbeaf", 0, sender=accounts[0])
 
 def test_fail_approval_bad_caller(supersig_with_proposal, accounts):
     with pytest.raises(ContractLogicError):
@@ -69,7 +95,7 @@ def test_fail_approve_twice(supersig_with_proposal, accounts):
     with pytest.raises(ContractLogicError):
         supersig_with_proposal.approve(1, sender=accounts[0])
 
-def test_fail_execute_to_few_approvals(supersig_with_proposal, accounts):
+def test_fail_execute_too_few_approvals(supersig_with_proposal, accounts):
     ## approve once
     supersig_with_proposal.approve(1, sender=accounts[0])
     ## try to approve again
@@ -79,3 +105,23 @@ def test_fail_execute_to_few_approvals(supersig_with_proposal, accounts):
 def test_fail_execute_does_not_exist(supersig, accounts):
     with pytest.raises(ContractLogicError):
         supersig.execute(0, sender=accounts[0])
+
+def test_fail_execute_twice(supersig, accounts):
+    accounts[0].transfer(supersig, value=42)
+    assert supersig.balance == 42
+    supersig.propose(3, accounts[4].address, '0x0', 42, sender=accounts[0])
+    supersig.approve(3, sender=accounts[0])
+    supersig.approve(3, sender=accounts[1])
+    supersig.approve(3, sender=accounts[2])
+
+    prev_balance = accounts[4].balance
+    supersig.execute(3, sender=accounts[0])
+    assert accounts[4].balance == prev_balance + 42
+
+    ## try to execute again
+    with pytest.raises(ContractLogicError):
+        supersig.execute(3, sender=accounts[0])
+
+def test_fail_revoke_no_approval(supersig, accounts):
+    with pytest.raises(ContractLogicError):
+        supersig.revoke_approval(1, sender=accounts[0])
